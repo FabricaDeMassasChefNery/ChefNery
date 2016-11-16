ff3eb4b71422d4b8a1a352ec8ad8c63fffc616be
MiguelNery <nery.mik@gmail.com>
2016-11-16 21:56:17 -0200

    updated product attributes gathering

--- chefenery/db/migrate/20161115233401_create_products.rb
+++ chefenery/db/migrate/20161115233401_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.string :category
+      t.integer :category
       t.decimal :price
       t.integer :quantity
 
