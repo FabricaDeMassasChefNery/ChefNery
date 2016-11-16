2ec799f4972e64397bb6109fabde02903f5bbfe1
miguelpimentel <miguelpimentel2013@gmail.com>
2016-11-16 11:46:23 -0200

    Correcting routes bug

--- chefenery/config/routes.rb
+++ chefenery/config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  #get 'produtos/about'  
-  #get 'startup/index'
-  #get 'produtos/contact'
+  root "home#index"
+
+  get 'home/about'
+  get 'home/contact'
+
+  
 
   devise_for :users
 
-  root "home#index"
-
-  get 'startup/index'
   get "home" => "home#index"
   get "cadastrar" => "devise/registrations#new"
   get "login" => "devise/sessions#new"
